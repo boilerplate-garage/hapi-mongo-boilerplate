34f1a7334339d13fef6a5842283ba483390cd829
IzumiSy <beetle-noise@gmx.com>
2016-02-02 13:58:23 -0800

    Implement GET /items and some more skelton code for APIs

--- controllers/item.js
+++ controllers/item.js
@@ -1,13 +1,32 @@
+var mongoConnect = function(req, collectionName) {
+  var db = req.server.plugins['hapi-mongodb'].db;
+  return db.collection(collecitonName);
+}
+
 module.exports = {
   itemGetAllAction: function(request, reply) {
-    return reply("GET /items");
+    var db = mongoConnect(request, "items");
+
+    db.find().toArray(function(err, doc) {
+      return reply(doc);
+    });
+
+    console.log("GET /items");
   },
 
   itemGetOneAction: function(request, reply) {
-    return reply("GET /items");
+    var db = mongoConnect(request, "items");
+
+    console.log("GET /item/" + request.params.id);
+
+    return reply("GET /item/{id}");
   },
 
   itemPostAction: function(request, reply) {
+    var db = mongoConnect(request, "items");
+
+    console.log("POST /item");
+
     return reply("POST /item");
   }
 };
